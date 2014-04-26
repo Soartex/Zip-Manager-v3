cfe64877b9616ff9195e75d418082de25ffc7f1b
goldbattle <patfgeneva@gmail.com>
2014-04-26 13:41:05 -0400

    Multi User Login
    - Thanks to @artdude543

--- login/VerifyLogin.php
+++ login/VerifyLogin.php
@@ -1,12 +1,23 @@
 <?php
-	if (isset($_POST['submit'])) {
-		// If password and username match continue
-		if ($_POST['username'] == 'username' && $_POST['password'] == 'password') {
+  
+  // Data hash arrays
+  $users = array(1 => 'username', 2 => 'username2', 3 => 'username3');
+  $user_pass = array(1 => 'password', 2 => 'password2', 3 => 'password3');
+  $user_id = array(1 => 'Soartex', 2 => 'FYD', 3 => 'JSTR');
+  // Set default values if not sent
+  $userName = isset($_POST['username']) ? $_POST['username'] : '';
+  $password = isset($_POST['password']) ? $_POST['password'] : '';
+  
+  // See if the user is a user
+  if (in_array($userName, $users)) {
+    $userID = array_search($userName, $users);
+    // See if the user password matches the one sent
+    if ($password == $user_pass[$userID]) {
 			// If correct login your session
 			session_start();
 			$_SESSION['logged'] = TRUE;
-			$_SESSION['User_ID'] = $_POST['username'];
-			// Redirect to the home pages
+			$_SESSION['User_ID'] = $user_id[$userID];
+			// Redirect to the option page
 			header("Location: ../options/");
 			exit;
 		}
